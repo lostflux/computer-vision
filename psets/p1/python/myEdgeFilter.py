66579b5dede9320ca1276376b62708cb22c28a4c
siavava <amittaijoel@outlook.com>
2023-01-19 23:05:32 -0500

    hw1 init

---
+++ psets/p1/python/myEdgeFilter.py
@@ -0,0 +1,8 @@
+import numpy as np
+from scipy import signal    # For signal.gaussian function
+
+from myImageFilter import myImageFilter
+
+def myEdgeFilter(img0, sigma):
+	# YOUR CODE HERE
+	pass
---
+++ psets/p1/python/myHoughLines.py
@@ -0,0 +1,6 @@
+import numpy as np
+import cv2  # For cv2.dilate function
+
+def myHoughLines(H, nLines):
+	# YOUR CODE HERE
+	pass
