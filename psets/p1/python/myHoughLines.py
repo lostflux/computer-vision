15ef2468e215bfdadf25756d5487ef3accf14df2
siavava <amittaijoel@outlook.com>
2023-01-21 12:32:18 -0500

    progress... fixed hough transform, need to work on line detection

--- psets/p1/python/myHoughLines.py
+++ psets/p1/python/myHoughLines.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import cv2  # For cv2.dilate function
 
-def myHoughLines(H, nLines: int):
+from typing import Tuple, List
+
+NdArray = np.ndarray
+
+def myHoughLines(H: NdArray, nLines: int) -> Tuple[List, List]:
 	"""This is a function that finds the nLines strongest lines in the Hough
 
 	Args:
@@ -12,21 +19,24 @@ def myHoughLines(H, nLines: int):
 	rhos, thetas = [], []
 
 	# Create a copy of H
-	array_copy = np.copy(H)
+	# H = np.copy(H)
 	
-	threshold = 7
-	for line in range(nLines):
-		line_location = np.unravel_index(np.argmax(array_copy), array_copy.shape)
+	threshold = 2
+	for line_number in range(nLines):
+		line_candidate = np.argmax(H)
+		line_location = np.unravel_index(line_candidate, H.shape)
 		rhos.append(line_location[0])
 		thetas.append(line_location[1])
-		array_copy[line_location[0], line_location[1]] = 0
+		# H[line_location[0], line_location[1]] = 0
 
-		for adjacent_row in range(-threshold, threshold):
+		for adjacent_row in range(-threshold, threshold+1):
 			row_index = line_location[0] + adjacent_row
-			if 0 <= row_index < array_copy.shape[0]:
-				for adjacent_column in range(-threshold, threshold):
-					col_index = line_location[1] + adjacent_column
-					if 0 <= col_index < array_copy.shape[1]:
-						array_copy[row_index, col_index] = 0
+			if 0 <= row_index < H.shape[0]:
+				for adjacent_column in range(-threshold, threshold+1):
+					if adjacent_row == 0 and adjacent_column == 0:
+						col_index = line_location[1] + adjacent_column
+						if 0 <= col_index < H.shape[1]:
+							# do nothing if the point is the line candidate
+							H[row_index, col_index] = 0
 
 	return rhos, thetas
--- psets/p1/python/myHoughTransform.py
+++ psets/p1/python/myHoughTransform.py
@@ -3,25 +3,44 @@
 
 from typing import Tuple
 import numpy as np
+from math import sqrt
 
 NdArray = np.ndarray
 
-def myHoughTransform(image, rhoRes, thetaRes) -> Tuple[NdArray, NdArray, NdArray]:
+def myHoughTransform(image: NdArray, rhoRes, thetaRes) -> Tuple[NdArray, NdArray, NdArray]:
 	image_width, image_height = image.shape
-	diagonal_length = np.sqrt(image_width**2 + image_height**2)
+	diagonal_length = sqrt(image_width**2 + image_height**2)
 	M = diagonal_length + 1
-	rho_scale = np.arange(0, M, rhoRes)
-	theta_scale = np.arange(0, 2 * np.pi, thetaRes)
 
-	accumulator = np.zeros((len(rho_scale), len(theta_scale)))
-	
+	rho_bound = int(np.ceil(M / rhoRes))
+	theta_bound = int(np.ceil(2 * np.pi / thetaRes))
+
+	accumulator = np.zeros((rho_bound, theta_bound))
+
 	for row in range(image_width):
 		for column in range(image_height):
 			if image[row, column] > 0:
-				for theta_index, theta in enumerate(theta_scale):
-					rho = column * np.cos(theta) + row * np.sin(theta)
-					rho_index = np.argmin(np.abs(rho_scale - rho))
-					accumulator[rho_index, theta_index] += 1
+				for theta in range(theta_bound):
+					_theta = theta * thetaRes
+					rho = column * np.cos(_theta) + row * np.sin(_theta)
+					
+					if rho >= 0:
+						rho_index = int(rho // rhoRes)
+						accumulator[rho_index, theta] += 1
+
+	rho_scale = np.arange(0, M, rhoRes)
+	theta_scale = np.arange(0, 2 * np.pi, thetaRes)
+
+	# accumulator = np.zeros((len(rho_scale), len(theta_scale)))
+	
+	# for row in range(image_width):
+	# 	for column in range(image_height):
+	# 		if image[row, column] > 0:
+	# 			for theta in theta_scale:
+	# 				rho = column * np.cos(theta) + row * np.sin(theta)
+	# 				rho_index = np.argmin(np.abs(rho_scale - rho))
+	# 				theta_index = np.argmin(np.abs(theta_scale - theta))
+	# 				accumulator[rho_index, theta_index] += 1
 
 	return accumulator, rho_scale, theta_scale
 
